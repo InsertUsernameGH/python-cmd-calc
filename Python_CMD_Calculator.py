5ba55dc7a7da3e08501a90fc66823ad412b26096
InsertUsernameGH <39683371+InsertUsernameGH@users.noreply.github.com>
2018-07-04 16:43:43 +1000

    Python CMD Calculator Version 1.2
    extended the help command

--- Python_CMD_Calculator.py
+++ Python_CMD_Calculator.py
@@ -16,9 +16,17 @@ say = 0
 print("Welcome to the Python CMD Calculator! Type help for a list of commands. ")
 while True:
     command = input(">>> ")
+    # This is the help command. Help starting now!
     if command == "help":
-        print("This is the Command Line Calculator, or as I like to call it, the CMDCalc. This is a list of commands: ")
-        print("=======================================================================================================")
+        print("This is the Command Line Calculator, made by InsertUsernameGH. (That's me.) ")
+        print("===============================================================================")
+        print("Type 'help math' for a list of maths commands.")
+        print("Type 'help fun' for a list of fun commands.")
+        print("Type 'help version' for version information.")
+        print("Type 'help author' for information about the author.")
+
+    if command == "help math":
+        print("Maths commands:")
         print("'add' - This command allows you to add two numbers together.")
         print("'subtract' - This command allows you to subtract one number from another.")
         print("'multiply' - This command allows you to multiply two numbers.")
@@ -29,17 +37,32 @@ while True:
         print("'modulus' - This command allows you to find the modulus of two numbers.")
         print("'double' - This command allows you to double a number.")
         print("'square_root' - This command allows you to find the square root of a number.")
-        print(" ")
+
+    if command == "help fun":
         print("Fun commands: ")
         print("'say' - This command allows the Python CMDCalc to print something of your choice.")
-        print(" ")
-        print("More commands are coming soon!")
-        print("Check out my blog! littlebuglet.wordpress.com")
+
+    if command == "help version":
+        print("Python Command Line Calculator (PCMLC)")
+        print("========================================")
+        print("Version 1.2")
+        print("Python version: 3.6.5")
+        print("========================================")
+    if command == "help author":
+        print("InsertUsername")
+        print("=========================")
+        print("YouTube: InsertUsernameYT")
+        print("SoundCloud: InsertUsernameSC")
+        print("GitHub: InsertUsernameGH")
+        print("Twitter: @littlebugletYT")
+        print("Discord: InsertUsername#1094")
+        print("Discord Server: https://discord.gg/y9Cva53")
+# Aaaaand that's the end of the help commands.
         
     if command == "add":
         number_1 = input("First number: ")
         number_2 = input("Second number: ")
-        output_number = int(number_1) + int(number_2)
+        output_number = int(number_1) + int(number_2.)
         print(output_number)
         
     if command == "subtract":
@@ -118,4 +141,5 @@ while True:
         output_number = int(number_1) * 2
         print(output_number)
                                 
+
 
